8923f3b9572e4798bdda81995f530ad277f15cb8
Mike The Madkiwi <mike@hildyard.ca>
2025-08-05 17:06:01 -0400

    minor alteration to disclude my core and add inventory example

--- cl_editable.lua
+++ cl_editable.lua
@@ -1,17 +1,6 @@
 --------------------------------------------------------------------------------------------
 -- ADD VORP / QBCORE / OTHER ClientSide IMPORTS HERE
 --------------------------------------------------------------------------------------------
-mc = exports["mad-core"]:Get()
-AddEventHandler("mad:cl_onOutdated", function()
-	mc = exports["mad-core"]:Get()
-end)
-madData = DatabindingGetDataContainerFromPath("madContainer")
-
-
-
-
-
-
 
 
 --------------------------------------------------------------------------------------------
@@ -24,7 +13,7 @@ debugClientShow = false
 -- updateInventory - is passed a tag Hash id, making it possible for your inventory system to add the sample from the tag.
 function ClientInventoryAdd(TagHashId)
     -- this taghashid is the EXACT itemhashid of the sample you need to add to the system.
-    mc.Satchel.AddItem(TagHashId, 1)
+    -- YOURINVENTORYSYSTEM.AddItem(TagHashId, 1) <<- example of how you'd use it
 end
 
 function ClientReviveHandler(ReviveData)
